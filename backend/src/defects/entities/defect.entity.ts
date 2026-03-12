03a69c1b5a6dd09f24c6896a32d3418e98f8a944
Aree <66160062@go.buu.ac.th>
2026-03-12 16:09:53 +0700

    feat(defect): implement Defect entity with no releations yet

--- backend/src/defects/entities/defect.entity.ts
+++ backend/src/defects/entities/defect.entity.ts
@@ -1,1 +1,31 @@
-export class Defect {}
+import { Column, Entity, PrimaryGeneratedColumn , CreateDateColumn , UpdateDateColumn , DeleteDateColumn, ManyToMany, ManyToOne } from "typeorm"
+
+@Entity('defect')
+export class Defect {
+    @PrimaryGeneratedColumn()
+    defect_id: number;
+    
+    @Column({ type: 'text'})
+    description: string;
+
+    @Column({ type: 'varchar', length: 50 })
+    severity: string;
+
+    @Column({ type: 'varchar', length: 255 , default: '/defect-images/unknown.jpg'  })
+    image_url: string;
+
+    @Column()
+    image_file_size: number;
+
+    @Column({ type: 'varchar', length: 50 , default: 'found' })
+    status: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    @DeleteDateColumn()
+    deleted_at: Date;
+}
